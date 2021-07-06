d00a864e74b175cc08af1e316f676aa4a7208411
Christian Jairo Sarmiento <55115040+jairo8925@users.noreply.github.com>
2021-07-06 02:23:22 -0700

    added sound when keyboard associated with piano key is pressed

--- Virtual Piano/task/src/scripts.js
+++ Virtual Piano/task/src/scripts.js
@@ -1,8 +1,7 @@
 document.addEventListener("keydown", function(event) {
     const pianoCodes = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ"];
     if (pianoCodes.includes(event.code)) {
-        console.log(`The '${event.key}' key is pressed.`);
-    } else {
-        console.log(`Warning. The key '${event.key}' is pressed.`);
+        new Audio(`white_keys/${event.key.toUpperCase()}.mp3`).play();
     }
 });
+
